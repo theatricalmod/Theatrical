c50903fdf95b63588c3e401ec4bf76929ee83a6f
Stuart Pomeroy <rushmead@gmail.com>
2025-07-22 22:56:10 +0100

    Allow redstone interface to be configured by configuration card

--- common/src/main/java/dev/imabad/theatrical/blocks/interfaces/RedstoneInterfaceBlock.java
+++ common/src/main/java/dev/imabad/theatrical/blocks/interfaces/RedstoneInterfaceBlock.java
@@ -1,16 +1,27 @@
 package dev.imabad.theatrical.blocks.interfaces;
 
+import dev.imabad.theatrical.TheatricalScreen;
 import dev.imabad.theatrical.blockentities.interfaces.RedstoneInterfaceBlockEntity;
+import dev.imabad.theatrical.blockentities.light.BaseDMXConsumerLightBlockEntity;
 import dev.imabad.theatrical.blocks.Blocks;
 import dev.imabad.theatrical.client.gui.screen.GenericDMXConfigurationScreen;
+import dev.imabad.theatrical.dmx.DMXNetwork;
+import dev.imabad.theatrical.dmx.DMXNetworkData;
+import dev.imabad.theatrical.items.Items;
+import dev.imabad.theatrical.net.OpenScreen;
+import dev.imabad.theatrical.util.UUIDUtil;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -40,11 +51,36 @@ public class RedstoneInterfaceBlock  extends Block implements EntityBlock {
     }
 
     @Override
-    @Environment(EnvType.CLIENT)
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        if(level.isClientSide){
-            RedstoneInterfaceBlockEntity be = (RedstoneInterfaceBlockEntity)level.getBlockEntity(pos);
-            Minecraft.getInstance().setScreen(new GenericDMXConfigurationScreen<>(be, pos, "block.theatrical.redstone_interface"));
+        if(!level.isClientSide()) {
+            BlockEntity be = level.getBlockEntity(pos);
+            if (be instanceof RedstoneInterfaceBlockEntity redstoneInterfaceBlockEntity) {
+                if (!redstoneInterfaceBlockEntity.getNetworkId().equals(UUIDUtil.NULL)) {
+                    DMXNetwork network = DMXNetworkData.getInstance(level.getServer().overworld()).getNetwork(redstoneInterfaceBlockEntity.getNetworkId());
+                    if (network != null && !network.isMember(player.getUUID())) {
+                        return InteractionResult.FAIL;
+                    }
+                }
+                if (player.getItemInHand(hand).getItem() == Items.CONFIGURATION_CARD.get()) {
+                    ItemStack itemInHand = player.getItemInHand(hand);
+                    CompoundTag tagData = itemInHand.getOrCreateTag();
+                    redstoneInterfaceBlockEntity.setNetworkId(tagData.getUUID("network"));
+                    if (tagData.getBoolean("universeEnabled")) {
+                        redstoneInterfaceBlockEntity.setUniverse(tagData.getInt("dmxUniverse"));
+                    }
+                    if (tagData.getBoolean("addressEnabled")) {
+                        redstoneInterfaceBlockEntity.setChannelStartPoint(tagData.getInt("dmxAddress"));
+                    }
+                    if (tagData.getBoolean("autoIncrement")) {
+                        tagData.putInt("dmxAddress", tagData.getInt("dmxAddress") + redstoneInterfaceBlockEntity.getChannelCount());
+                    }
+                    itemInHand.save(tagData);
+                    DMXNetworkData instance = DMXNetworkData.getInstance(level.getServer().overworld());
+                    player.sendSystemMessage(Component.translatable("item.configurationcard.success", instance.getNetwork(redstoneInterfaceBlockEntity.getNetworkId()).name(), Integer.toString(redstoneInterfaceBlockEntity.getUniverse()), Integer.toString(redstoneInterfaceBlockEntity.getChannelStart()), Integer.toString(tagData.getInt("dmxAddress"))));
+                    return InteractionResult.SUCCESS;
+                }
+                new OpenScreen(pos, TheatricalScreen.GENERIC_DMX).sendTo((ServerPlayer) player);
+            }
         }
         return InteractionResult.SUCCESS;
     }
